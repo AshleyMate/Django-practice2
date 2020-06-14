f604ac69f99c5ff2d22ba4f0e9ec2fda5716cf63
chanyoung <young9484825@gmail.com>
2020-06-15 00:20:10 +0900

    results

--- pr/polls/views.py
+++ pr/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from polls.models import Question
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from polls.models import Choice, Question
 
 
 def index(request):
@@ -11,3 +13,23 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question.id)
+    return render(request, 'polls/results.html', {'question': question})
